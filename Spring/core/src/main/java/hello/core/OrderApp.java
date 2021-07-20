2a4f4e898f9040a26f22f0da3c1917c27719c316
vedivero <zzangu0703@naver.com>
2021-07-20 22:58:48 +0900

    Change Spring from AppConfig

--- Spring/core/src/main/java/hello/core/OrderApp.java
+++ Spring/core/src/main/java/hello/core/OrderApp.java
@@ -7,6 +7,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
@@ -14,16 +16,20 @@ public class OrderApp {
 
         //MemberService memberService = new MemberServiceImpl(null);
         //OrderService orderService = new OrderServiceImpl(null,null);
+        //AppConfig appConfig = new AppConfig();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order = " + order.calculatePrice());
